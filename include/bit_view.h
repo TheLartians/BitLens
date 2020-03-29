874f563e39eb213398d7889d9055e856e305ec7f
Lars Melchior <TheLartians@users.noreply.github.com>
2020-03-29 19:58:25 +0200

    format

--- include/bit_view.h
+++ include/bit_view.h
@@ -3,53 +3,51 @@
 #include <climits>
 
 namespace bit_view {
-  
-  const size_t BITS_IN_BYTE = CHAR_BIT;
-  
-  template <class C> class Container {
-  protected:
-    C & container;
-  public:
-    using Word = typename C::value_type;
-    static const size_t WORD_SIZE = sizeof(Word) * BITS_IN_BYTE;
-    
-    Container(C &c):container(c){  }
-    
-    /**
-     * returns the number of bits in the container
-     */
-    size_t size() {
-      return container.size() * WORD_SIZE;
-    }
-    
-    /**
-     * resizes the container to hold at least `N` bits.
-     */
-    void resizeToHold(size_t N, bool v = false) {
-      Word offset = N % WORD_SIZE == 0 ? 0 : 1;
-      container.resize(N / WORD_SIZE + offset, v);
-    }
-    
-    /**
-     * returns the value of the `i`th bit.
-     */
-    bool operator[](size_t i) const {
-      Word offset = i % WORD_SIZE;
-      return (container[i / WORD_SIZE] >> offset) & Word(1);
-    }
-    
-    /**
-     * sets the value of the `i`th bit
-     */
-    void set(size_t i, bool v) {
-      Word offset = i % WORD_SIZE;
-      if (v) {
-        container[i / WORD_SIZE] |= Word(1) << offset;
-      } else {
-        container[i / WORD_SIZE] &= ~(Word(1) << offset);
-      }
-    }
-    
-  };
 
-}
+const size_t BITS_IN_BYTE = CHAR_BIT;
+
+template <class C> class Container {
+protected:
+  C &container;
+
+public:
+  using Word = typename C::value_type;
+  static const size_t WORD_SIZE = sizeof(Word) * BITS_IN_BYTE;
+
+  Container(C &c) : container(c) {}
+
+  /**
+   * returns the number of bits in the container
+   */
+  size_t size() { return container.size() * WORD_SIZE; }
+
+  /**
+   * resizes the container to hold at least `N` bits.
+   */
+  void resizeToHold(size_t N, bool v = false) {
+    Word offset = N % WORD_SIZE == 0 ? 0 : 1;
+    container.resize(N / WORD_SIZE + offset, v);
+  }
+
+  /**
+   * returns the value of the `i`th bit.
+   */
+  bool operator[](size_t i) const {
+    Word offset = i % WORD_SIZE;
+    return (container[i / WORD_SIZE] >> offset) & Word(1);
+  }
+
+  /**
+   * sets the value of the `i`th bit
+   */
+  void set(size_t i, bool v) {
+    Word offset = i % WORD_SIZE;
+    if (v) {
+      container[i / WORD_SIZE] |= Word(1) << offset;
+    } else {
+      container[i / WORD_SIZE] &= ~(Word(1) << offset);
+    }
+  }
+};
+
+} // namespace bit_view
--- test/bit_view.cpp
+++ test/bit_view.cpp
@@ -1,60 +1,59 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-#include <doctest/doctest.h>
-#include <type_traits>
 #include <bit_view.h>
 #include <bitset>
+#include <doctest/doctest.h>
+#include <type_traits>
 
 TEST_CASE_TEMPLATE("bit_view", T, char, int, unsigned, size_t) {
   std::vector<T> container;
   bit_view::Container bits(container);
   static_assert(std::is_same<typename decltype(bits)::Word, T>::value);
   static_assert(bit_view::BITS_IN_BYTE >= 8);
-  const auto wordSize = sizeof(T)*bit_view::BITS_IN_BYTE;
+  const auto wordSize = sizeof(T) * bit_view::BITS_IN_BYTE;
 
-  SUBCASE("read bits"){
+  SUBCASE("read bits") {
     container.resize(2);
-    CHECK(bits.size() == container.size()*wordSize);
+    CHECK(bits.size() == container.size() * wordSize);
 
-    std::bitset<sizeof(T)*bit_view::BITS_IN_BYTE> bitset;
+    std::bitset<sizeof(T) * bit_view::BITS_IN_BYTE> bitset;
     bitset[1] = 1;
     bitset[3] = 1;
-    bitset[wordSize-1] = 1;
+    bitset[wordSize - 1] = 1;
 
     container[0] = static_cast<T>(bitset.to_ulong());
-    for (size_t i=0; i<wordSize; ++i) {
+    for (size_t i = 0; i < wordSize; ++i) {
       CAPTURE(i);
       CHECK(bits[i] == bitset[i]);
     }
-    
+
     bitset[2] = 1;
     bitset[3] = 0;
-    bitset[wordSize-2] = 1;
+    bitset[wordSize - 2] = 1;
     container[1] = static_cast<T>(bitset.to_ulong());
-    for (size_t i=0; i<wordSize; ++i) {
+    for (size_t i = 0; i < wordSize; ++i) {
       CAPTURE(i);
-      CHECK(bits[wordSize+i] == bitset[i]);
+      CHECK(bits[wordSize + i] == bitset[i]);
     }
   }
-  
+
   SUBCASE("resize") {
-    bits.resizeToHold(5*wordSize-3);
-    CHECK(bits.size() == 5*wordSize);
+    bits.resizeToHold(5 * wordSize - 3);
+    CHECK(bits.size() == 5 * wordSize);
     CHECK(container.size() == 5);
   }
-  
-  SUBCASE("set bits"){
-    bits.resizeToHold(5*wordSize);
-    for (size_t i=0; i<bits.size(); ++i) {
+
+  SUBCASE("set bits") {
+    bits.resizeToHold(5 * wordSize);
+    for (size_t i = 0; i < bits.size(); ++i) {
       CHECK(bits[i] == 0);
     }
-    auto pattern = [](auto i){ return i%(i/wordSize+1) == 0; };
-    for (size_t i=0; i<bits.size(); ++i) {
+    auto pattern = [](auto i) { return i % (i / wordSize + 1) == 0; };
+    for (size_t i = 0; i < bits.size(); ++i) {
       bits.set(i, pattern(i));
     }
-    for (size_t i=0; i<bits.size(); ++i) {
+    for (size_t i = 0; i < bits.size(); ++i) {
       CHECK(bits[i] == pattern(i));
     }
   }
-  
 }
